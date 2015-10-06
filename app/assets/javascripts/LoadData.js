ec545fe1d3048c963607890e880b1c9ca8f88aa8
Nisarg Desai <nisarg.desai@mavs.uta.edu>
2015-10-06 00:54:22 -0500

    Demo

---
+++ app/assets/javascripts/LoadData.js
@@ -0,0 +1,16 @@
+
+	$("button").click(function(){
+		
+			$.getJSON("welcome/DataFile.json",
+				function (json) {
+				var tr;
+				for (var i = 0; i < json.length; i++) {
+					tr = $('<tr/>');
+					tr.append("<td>" + json[i].city + "</td>");
+					tr.append("<td>" + json[i].Latitude + "</td>");
+					tr.append("<td>" + json[i].Longitude + "</td>");
+					$('table').append(tr);
+				}
+    });
+	});
+		
