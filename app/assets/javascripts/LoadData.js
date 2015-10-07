ab01e5619b7c504eccf06b6e69bdd07ae83563ce
Nisarg Desai <nisarg.desai@mavs.uta.edu>
2015-10-06 21:25:03 -0500

    Demo

--- app/assets/javascripts/LoadData.js
+++ app/assets/javascripts/LoadData.js
@@ -1,4 +1,4 @@
- $(document).ready(function() {
+$(document).ready(function() {
  
             $('#button').click(function(){
 
@@ -10,40 +10,15 @@
 
                         var json = $.parseJSON(data);
                         
-                       	var tr;
-				for (var i = 0; i < json.length; i++) {
-					tr = $('<tr/>');
-					tr.append("<td>" + json[i].city + "</td>");
-					tr.append("<td>" + json[i].Latitude + "</td>");
-					tr.append("<td>" + json[i].Longitude + "</td>");
-					$('table').append(tr);
-				}
-                    intialize(json);
-
+                        var tr;
+                for (var i = 0; i < json.length; i++) {
+                    tr = $('<tr/>');
+                    tr.append("<td>" + json[i].city + "</td>");
+                    tr.append("<td>" + json[i].Latitude + "</td>");
+                    tr.append("<td>" + json[i].Longitude + "</td>");
+                    $('table').append(tr);
+                }
                     }
                 });
             });
         });
-
-function initialize(json){
-
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-    var mapOptions = {
-        mapTypeId: 'roadmap'
-    };
-
-    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    map.setTilt(45);
-
-    var marker,i;
-
-    for( i = 0; i < json.length; i++ ) {
-        var position = new google.maps.LatLng(json[i].Latitude, json[i].Longitude);
-        bounds.extend(position);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map
-        });
-}
-
